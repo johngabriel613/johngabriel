de63e77ae2bff53d00156ff9e1ca996686357d19
johngabriel613 <hakdogdehakdog@gmail.com>
2021-12-04 18:51:14 +0800

    update educ

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 var menuBtn = document.querySelector('.menu-toggle-open')
 var menuBtn2 = document.querySelector('.menu-toggle-close')
 var navBar = document.querySelector('.nav-bar')
-let menuOpen = false;
+
 
 menuBtn.addEventListener('click', () => {
     navBar.classList.add('active')
     menuBtn.classList.add('slide')
     Home.classList.add('blur')
     About.classList.add('blur')
-    menuOpen = true;
+    Education.classList.add('blur')
+    Logo.classList.add('blur')
+    
 })
 
 menuBtn2.addEventListener('click', () => {
@@ -17,7 +19,9 @@ menuBtn2.addEventListener('click', () => {
     menuBtn.classList.remove('slide')
     Home.classList.remove('blur')
     About.classList.remove('blur')
-    menuOpen = false;
+    Education.classList.remove('blur')
+    Logo.classList.remove('blur')
+    
 })
 
 //Page Nav
@@ -25,6 +29,8 @@ var Home = document.querySelector('.home')
 var Homebtn = document.querySelector('.home-button')
 var About = document.querySelector('.about')
 var Aboutbtn = document.querySelector('.abt-btn')
+var Education = document.querySelector('.education')
+var Educbtn = document.querySelector('.educ-btn')
 var Menudark = document.querySelector('.color')
 var Logo = document.querySelector('.logo')
 var NavBckgrnd = document.querySelector('.nav-bg')
@@ -34,6 +40,8 @@ Homebtn.addEventListener('click', () => {
     Homebtn.classList.add('present')
     About.classList.remove('active')
     Aboutbtn.classList.remove('present')
+    Education.classList.remove('active')
+    Educbtn.classList.remove('present')
     Menudark.classList.remove('dark')
     Logo.classList.remove('dark')
     NavBckgrnd.classList.remove('white')
@@ -41,6 +49,8 @@ Homebtn.addEventListener('click', () => {
     menuBtn.classList.remove('slide')
     Home.classList.remove('blur')
     About.classList.remove('blur')
+    Education.classList.remove('blur')
+    Logo.classList.remove('blur')
 
 })
 
@@ -49,6 +59,8 @@ Aboutbtn.addEventListener('click', () => {
     Homebtn.classList.remove('present')
     About.classList.add('active')
     Aboutbtn.classList.add('present')
+    Education.classList.remove('active')
+    Educbtn.classList.remove('present')
     Menudark.classList.add('dark')
     Logo.classList.add('dark')
     NavBckgrnd.classList.add('white')
@@ -56,6 +68,26 @@ Aboutbtn.addEventListener('click', () => {
     menuBtn.classList.remove('slide')
     Home.classList.remove('blur')
     About.classList.remove('blur')
+    Education.classList.remove('blur')
+    Logo.classList.remove('blur')
+})
+
+Educbtn.addEventListener('click', () => {
+    Home.classList.remove('active')
+    Homebtn.classList.remove('present')
+    About.classList.remove('active')
+    Aboutbtn.classList.remove('present')
+    Education.classList.add('active')
+    Educbtn.classList.add('present')
+    Menudark.classList.add('dark')
+    Logo.classList.add('dark')
+    NavBckgrnd.classList.add('white')
+    navBar.classList.remove('active')
+    menuBtn.classList.remove('slide')
+    Home.classList.remove('blur')
+    About.classList.remove('blur')
+    Education.classList.remove('blur')
+    Logo.classList.remove('blur')
 })
 
 //Typing Animation
